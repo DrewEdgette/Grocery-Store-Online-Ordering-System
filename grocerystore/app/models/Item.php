225e7a575a2e37cf1310ffa6259ecbc6ab9b2cb0
DrewEdgette <49006910+DrewEdgette@users.noreply.github.com>
2021-03-16 19:18:36 -0400

    Update Item.php

--- grocerystore/app/models/Item.php
+++ grocerystore/app/models/Item.php
@@ -6,7 +6,7 @@
             $this->db = new Database;
         }
 
-        // creates new user in database and then returns true or false
+        // searches database for requested item
         public function getSearchResults($data) {
             $this->db->query("SELECT * FROM items WHERE item_name LIKE CONCAT('%', :query, '%');");
             $this->db->bind(":query", $data["query"]);
